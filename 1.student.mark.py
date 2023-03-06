49f6978f0a7af1617996eacdd9c0118193303070
wynnn3301 <minhduc8374@gmail.com>
2023-03-06 20:40:41 +0700

    Add 3 more print functions, the main code is used to test the functions

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,37 +1,68 @@
-def nhap_hocsinh(danhsach_hocsinh):   # Ham nhap so hoc sinh
-    so_hoc_sinh = int(input('Nhap so hoc sinh: '))
-    for i in range(so_hoc_sinh):
-        small_hs = []
-        id_hs = input('Nhap id hs: ')
-        ten_hs = input('Nhap ten hs: ')
-        dob_hs = input('Nhap dob hs: ')
-        small_hs.append(id_hs)
-        small_hs.append(ten_hs)
-        small_hs.append(dob_hs)
-        danhsach_hocsinh.append(small_hs)
+def input_stu(stu_list):   
+    num_stu = int(input('Nhap so hoc sinh: '))
+    for i in range(num_stu):
+        small_list_stu = []
+        id_stu = input('Nhap id hs: ')
+        stu_name = input('Nhap ten hs: ')
+        stu_dob = input('Nhap dob hs: ')
+        small_list_stu.append(id_stu)
+        small_list_stu.append(stu_name)
+        small_list_stu.append(stu_dob)
+        stu_list.append(small_list_stu)
 
 
-def nhap_khoa_hoc (khoa_hoc):   # Ham nhap thong tin cua khoa hoc
-    so_kh = int(input('So khoa hoc: '))
-    for i in range(so_kh): 
-        small_kh = []
-        id_kh = input('ID cua khoa hoc: ')
-        ten_kh = input('Ten khoa hoc: ')
-        small_kh.append(id_kh)
-        small_kh.append(ten_kh)
-        khoa_hoc.append(small_kh)
+def input_course (course):   
+    num_course = int(input('So khoa hoc: '))
+    for i in range(num_course): 
+        small_list_course = []
+        id_course = input('ID cua khoa hoc: ')
+        course_name = input('Ten khoa hoc: ')
+        small_list_course.append(id_course)
+        small_list_course.append(course_name)
+        course.append(small_list_course)
     
 
-def chon_va_nhapdiem (khoa_hoc, small_kh):  # Ham chon khoa hoc va nhap diem
-    pick_kh = input('nhap khoa hoc muon chon: ')
-    for i in list(khoa_hoc):                # Loop trong list
-        for i in list(small_kh):            # Loop trong list be
-            if pick_kh == small_kh[0]:
-                dict_diem = {}
-                diem = int(input('Nhap diem cua hs: '))
-                dict_diem['diem'] = diem
-                khoa_hoc.append(dict_diem)
+def pick_cs_and_enter_mark (course, stu_list):  
+    pick_cs = input('nhap khoa hoc muon chon: ')
+    for small_list_course in course:                
+            if pick_cs == small_list_course[0]:
+                mark = []
+                for i in range(len(stu_list)):
+                    point = int(input('Nhap point cua hs: '))
+                    mark.append(point)
+                small_list_course.append(mark)
 
-danhsach_hocsinh = []
-khoa_hoc = []
 
+def print_stu (stu_list): 
+    print ('danh sach hoc sinh: ')
+    for small_list_stu in stu_list:
+        for i in range(len(small_list_stu)):    
+            print (small_list_stu[i] + ' ')
+
+
+def print_cs(course): 
+    print('danh sach khoa hoc: ')
+    for small_list_course in course:
+        for i in small_list_course:
+            try: 
+               print(i + ' ')
+            except TypeError:
+                pass
+
+def print_point(course):
+    enter_cs_id = input('Nhap id khoa hoc can xem diem: ')
+    for small_list_course in course:
+        if small_list_course[0] == enter_cs_id:
+            print('diem trong khoa hoc', small_list_course[2])
+        
+
+
+# Main code
+stu_list = []
+course = []
+input_stu(stu_list)
+input_course(course)
+pick_cs_and_enter_mark(course,stu_list)
+print_stu(stu_list)
+print_cs(course)
+print_point(course)
